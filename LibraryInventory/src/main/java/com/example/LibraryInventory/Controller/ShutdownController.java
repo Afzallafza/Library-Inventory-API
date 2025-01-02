cce74a291b6903244a84a1945db1abc492617f16
afzallafza <afzallafza@gmail.com>
2025-01-02 14:01:24 +0600

    Added Exit Controller

---
+++ LibraryInventory/src/main/java/com/example/LibraryInventory/Controller/ShutdownController.java
@@ -0,0 +1,27 @@
+package com.example.LibraryInventory.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShutdownController {
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    @PostMapping("/exit")
+    public String exitApplication() {
+        Thread shutdownThread = new Thread(() -> {
+            try {
+                Thread.sleep(1000); 
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            context.close(); 
+        });
+        shutdownThread.start();
+        return "Application is shutting down gracefully...";
+    }
+}
